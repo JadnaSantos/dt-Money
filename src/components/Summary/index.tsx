d01ae87951476d395d4351825564e83e24e18c10
Jadna Santos <jadnajesus7@gmail.com>
2022-10-24 15:58:55 -0300

    inital-commit

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,40 @@
+import { SummaryCard, SummaryContainer } from './styles'
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react'
+
+import { priceFormat } from '../../utils/formatter'
+import { useSummary } from '../../hooks/useSummary'
+
+export function Summary() {
+  const summary = useSummary()
+
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entrada</span>
+          <ArrowCircleUp size={32} color="#00b373" />
+        </header>
+
+        <strong>{priceFormat.format(summary.income)}</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+
+        <strong>{priceFormat.format(summary.outcome)}</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#FFF" />
+        </header>
+
+        <strong>{priceFormat.format(summary.outcome)}</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
